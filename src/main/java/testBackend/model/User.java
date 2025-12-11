c1ac41db67c473a097ed6e5ff7899b6fb10800bc
Anderson Oliveira <annderoli@gmail.com>
2025-12-11 11:32:59 -0300

    Criado Entity

--- src/main/java/testBackend/model/User.java
+++ src/main/java/testBackend/model/User.java
@@ -1,4 +1,48 @@
 package testBackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String cpf;
+    private String email;
+    private String password;
+
+    public User() {}
+
+    public User(String username, String cpf, String email, String password) {
+        this.username = username;
+        this.cpf = cpf;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
